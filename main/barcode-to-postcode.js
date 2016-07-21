756a855e4af6fab56ee63922b14052c81d8009e4
XLuoChen <1425145741@qq.com>
2016-07-22 01:25:32 +0800

    realize convert barcode-to-postcode

---
+++ main/barcode-to-postcode.js
@@ -0,0 +1,67 @@
+'use strict';
+
+function checkBarcode(barcode) {
+  const barcodeArray = barcode.split('');
+  return (barcode[0] === '|' && barcodeArray[barcodeArray.length - 1] === '|'
+  && ((barcodeArray.length - 2) / 5 === 6 || (barcodeArray.length - 2) / 5 === 10))
+
+    ? true : false;
+}
+
+function getBarcodes(barcode) {
+  const barcodes = [];
+  let start = 1;
+
+  while (start < barcode.length - 1) {
+
+    let substring = barcode.substring(start, start + 5);
+    barcodes.push(substring);
+    start += 5;
+  }
+  return barcodes;
+}
+
+function getDigits(barcodes, weight) {
+  return barcodes.map(barcode => {
+    const columns = barcode.split('');
+    let i = 0;
+    let sum = 0;
+    columns.map(column => {
+      sum += (column === '|' ? 1 : 0) * weight[i++];
+      sum = (sum === 11) ? 0 : sum;
+    });
+
+    return sum;
+  })
+}
+
+function loadWeight() {
+  return [7, 4, 2, 1, 0];
+}
+
+function checkDigits(digits) {
+  let sum = 0;
+
+  for (const digit of digits) {
+    sum += digit;
+  }
+
+  return (sum % 10 === 0) ? true : false;
+}
+
+function convertPostcode(digits) {
+  const digitsString = digits.join('');
+
+  return (digitsString.length > 5) ?
+    `${digitsString.substring(0, 5)}-${digitsString.substring(5, digitsString.length)}`
+    : `${digitsString.substring(0, 5)}`;
+}
+
+module.exports = {
+  checkBarcode: checkBarcode,
+  getBarcodes: getBarcodes,
+  getDigits: getDigits,
+  loadWeight: loadWeight,
+  checkDigits: checkDigits,
+  convertPostcode: convertPostcode
+};
---
+++ spec/barcode-to-postcode-spec.js
@@ -0,0 +1,85 @@
+'use strict';
+
+describe('convert barcode to postcode', () => {
+
+  const barcodeToPostcode = require('../main/barcode-to-postcode');
+
+  describe('checkBarcode', ()=> {
+    let barcode;
+
+    it('when input valid barcode should return false', () => {
+      barcode = ':|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|||:::';
+
+      const isCorrect = barcodeToPostcode.checkBarcode(barcode);
+
+      expect(isCorrect).toBe(false);
+    });
+
+    it('when input valid barcode should return false', () => {
+      barcode = ':|::|:|::|:|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|||:::';
+
+      const isCorrect = barcodeToPostcode.checkBarcode(barcode);
+
+      expect(isCorrect).toBe(false);
+    });
+
+    it('when input correct barcode should return true', () => {
+      barcode = '|:|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|||:::|';
+
+      const isCorrect = barcodeToPostcode.checkBarcode(barcode);
+
+      expect(isCorrect).toBe(true);
+    });
+  });
+
+  it('get barcodes', () => {
+    const barcode = '|:|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|||:::|';
+    const barcodes = barcodeToPostcode.getBarcodes(barcode);
+
+    expect(barcodes).toEqual([':|::|', ':|:|:', '||:::', ':|:|:', ':||::', ':::||', '::|:|', '::||:', ':|::|', '||:::']);
+  });
+
+  it('should get digits', () => {
+    const barcodes = [':|::|', ':|:|:', '||:::'];
+    const weight = barcodeToPostcode.loadWeight();
+    const digits = barcodeToPostcode.getDigits(barcodes, weight);
+
+    expect(digits).toEqual([4, 5, 0]);
+  });
+
+  describe('checkCodes', () => {
+    let digits;
+
+    it('when check success should return true', () => {
+      digits = [9, 5, 7, 1, 3, 5];
+      const isCorrect = barcodeToPostcode.checkDigits(digits);
+
+      expect(isCorrect).toEqual(true);
+    });
+
+    it('when check failed should return false', () => {
+      digits = [9, 5, 7, 1, 3, 4];
+      const isCorrect = barcodeToPostcode.checkDigits(digits);
+
+      expect(isCorrect).toEqual(false);
+    });
+  });
+
+  describe('should convert postcode', () => {
+    let digits;
+
+    it("if length of digits more than 5 should add '-'", () => {
+      digits = [4, 5, 0, 5, 6, 1, 2, 3, 4];
+      const postcode = barcodeToPostcode.convertPostcode(digits);
+
+      expect(postcode).toEqual('45056-1234');
+    });
+
+    it("if length of digits  equal 5 should convert", () => {
+      digits = [4, 5, 0, 5, 6];
+      const postcode = barcodeToPostcode.convertPostcode(digits);
+
+      expect(postcode).toEqual('45056');
+    });
+  });
+});
