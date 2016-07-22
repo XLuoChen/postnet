91d62e6c4a343e7e5e457dea0b19422ff4afb0ed
XLuoChen <1425145741@qq.com>
2016-07-22 17:06:54 +0800

    refactor

--- main/postcode-to-barcode.js
+++ main/postcode-to-barcode.js
@@ -2,22 +2,19 @@
 
 function checkPostcode(postcode) {
 
-  return (/^\d{5}$/.test(postcode) || /^\d{5}-\d{4}$/.test(postcode))
-    && (postcode.length === 10 || postcode.length === 5 || postcode.length === 9);
+  return (/^\d{5}$/.test(postcode) || /^\d{5}-\d{4}$/.test(postcode));
 }
 
 function getPostcodes(postcode) {
 
-  const postcodeArray = postcode.split('-');
-
-  return ((postcodeArray.length > 1) ?
-    postcodeArray[0].split('').concat(postcodeArray[1].split(''))
-    : postcodeArray[0].split('')).map(code => parseInt(code));
+  return postcode.replace('-', '')
+    .split('')
+    .map(code => parseInt(code));
 }
 
 function appendCheckCode(postcodes) {
 
-  let sum = postcodes.reduce((prev,curr) => prev + curr);
+  let sum = postcodes.reduce((prev, curr) => prev + curr);
   postcodes.push(10 - sum % 10);
 
   return postcodes;
@@ -32,7 +29,7 @@ function convertBarcode(appendedPostCodes, allBarcodes) {
 
 
 function loadAllBarcodes() {
-  return ['||:::',':::||', '::|:|', '::||:', ':|::|',
+  return ['||:::', ':::||', '::|:|', '::||:', ':|::|',
     ':|:|:', ':||::', '|:::|', '|::|:', '|:|::'];
 }
 
